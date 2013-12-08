6eac69c5c265b7fc3a8bed4149b1824a33dc341d
Christian Kniep <christian@qnib.org>
2013-12-08 23:25:36 +0100

    added ewma to consumer

--- cargo-relay.go
+++ cargo-relay.go
@@ -8,15 +8,31 @@ import (
         "fmt"
         "lang"
         "time"
+        "gcfg"
+        "github.com/VividCortex/ewma"
+
 )
 type Metric struct {
     Mkey string  // metric key
-    Mval int64   // metric value
+    Mval int   // metric value
     Mts  int     // metric unix-epoch
 }
 
+type Config struct {
+        Global struct {
+            Hosts string
+            Port string
+        }
+        Queue struct {
+            Max int
+        }
+}
+
 func main() {
-        ln, err := net.Listen("tcp", "0.0.0.0:6666")
+        var cfg Config
+        err := gcfg.ReadFileInto(&cfg, "cargo.conf")
+        address := fmt.Sprintf("%s:%s", []byte(cfg.Global.Hosts), []byte(cfg.Global.Port))
+        ln, err := net.Listen("tcp", address)
         if err != nil {
                 // handle error
         }
@@ -33,9 +49,15 @@ func main() {
 }
 
 func consume(queue *lang.Queue) {
+    e := ewma.NewMovingAverage()
     for {
-      time.Sleep(2000 * time.Millisecond)
-      fmt.Println("Total items: ", queue.Len())
+        old_len := queue.Len()
+        time.Sleep(1000 * time.Millisecond)
+        new_len := queue.Len()
+        msg_cnt := float64(new_len - old_len)
+        e.Add(msg_cnt)
+        str := fmt.Sprintf("Msg cnt: %v | Msg send: %v | ewma: %v", new_len, msg_cnt, e.Value())
+        fmt.Println(str)
     }
 }
 
@@ -49,14 +71,11 @@ func handle(conn net.Conn, queue *lang.Queue) {
         scanner := bufio.NewScanner(conn)
         for scanner.Scan() {
                 if m := strings.Split(scanner.Text(), " "); len(m) > 2 {
-                        if ts, err := strconv.ParseInt(m[2], 10, 0); err != nil {
-                                // handling warning
-                                continue
-                        } else {
-                                m := &Metric{"test.test1", 1, 2}
-                                fmt.Println("Metric ", ts, m)
-                                queue.Push(m)
-                        }
+                        ts,_ := strconv.ParseInt(m[2], 10, 0)
+                        val,_ := strconv.Atoi(m[1])
+                        m := &Metric{m[0], val, int(ts)}
+                        //fmt.Println("Metric ", ts, m)
+                        queue.Push(m)
                 }
         }
 
